b4354386b87262642e05e90af83a6d8817efa7c5
NathanGraddon <ngraddon@udel.edu>
2025-03-31 14:04:09 -0400

    Imports not being used are commented out so site can deploy.

--- src/components/BasicAssessment.tsx
+++ src/components/BasicAssessment.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import { Question } from "../interfaces/question";
+// import { Button } from "react-bootstrap";
+// import { Question } from "../interfaces/question";
 
 const questions = [
     {
